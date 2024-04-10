1964040fbacebe73f5b3b7c2f6da1cf5cae2ca4c
Nguyen Thi Toi <nguyentoi18092003hvcnbcvt@gmail.com>
2024-04-11 05:01:14 +0700

    Tinh nang xoa nhieu san pham

--- task-management/api/v1/controllers/task.controller.js
+++ task-management/api/v1/controllers/task.controller.js
@@ -83,6 +83,16 @@ module.exports.changeMulti=async(req,res)=>{
                     message:"Cập nhật trạng thái thành công!"
                 });
                 break;
+            case "deleted":
+                await Task.updateMany({_id:{$in:ids}},{
+                    deleted:true,
+                    deletedAt:new Date()
+                });
+                res.json({
+                    code:200,
+                    message:"Xóa thành công!"
+                });
+                break; 
             default:
                 res.json({
                     code:400,
