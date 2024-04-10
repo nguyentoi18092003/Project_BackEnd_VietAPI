a9d7fac64348890ad4f812506f44aafe7ddbd7ef
Nguyen Thi Toi <nguyentoi18092003hvcnbcvt@gmail.com>
2024-04-11 03:30:47 +0700

    Tinh nang thay doi trang thai san pham

--- task-management/index.js
+++ task-management/index.js
@@ -1,5 +1,6 @@
 // console.log("OK")
 const express=require("express");
+const bodyPaser=require("body-parser");
 require("dotenv").config();
 const database=require("./config/database");
 //Routes Ver1
@@ -7,10 +8,13 @@ const routesVer1=require("./api/v1/routes/index.route")
 
 database.connect();
 const app=express();
+const port=process.env.PORT;
 
+//parse application/json
+app.use(bodyPaser.json());
 //Routes Ver1
 routesVer1(app);
-const port=process.env.PORT;
+
 
 app.listen(port,()=>{
     console.log(`App listening on port ${port}`);
