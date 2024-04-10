72299c1f719557cac9210b520ed2c9251f3e7f32
Nguyen Thi Toi <nguyentoi18092003hvcnbcvt@gmail.com>
2024-04-11 01:28:53 +0700

    Bo loc theo trang thai

--- task-management/api/v1/controllers/task.controller.js
+++ task-management/api/v1/controllers/task.controller.js
@@ -1,28 +1,25 @@
 const Task=require("../models/task.model");
 //[GET]/api/v1/tasks
 module.exports.index= async(req,res)=>{
-    try{
-    const tasks=await Task.find({
+    const find={
         deleted:false
-    })
+    };
+
+    // Bộ lọc theo trang thai
+    if(req.query.status){
+        find.status=req.query.status;
+    }
+    // Hết bộ lọc theo trang thai
+    const tasks=await Task.find(find)
     res.json(tasks)
-}
-catch(error){
-    console.log(error)
-}
 }
 //[GET]/api/v1/tasks/detail/:id
 module.exports.detail=async(req,res)=>{
-    try{
         const id=req.params.id;
-        // console.log(id)
         const task= await Task.findOne({
             _id: id,
             deleted: false
         });
         res.json(task);
-    }
-    catch(error){
-        console.log(error)
-    }
+
 }
