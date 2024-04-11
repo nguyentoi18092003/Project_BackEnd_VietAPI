f1f4886fbff355caaf2b36f08deb7af41a909075
Nguyen Thi Toi <nguyentoi18092003hvcnbcvt@gmail.com>
2024-04-11 19:46:14 +0700

    Tinh nang dang nhap(luu ca token vao cookie)

---
+++ task-management/api/v1/routes/user.route.js
@@ -0,0 +1,6 @@
+const express=require("express");
+const router =express.Router();
+
+const controller=require("../controllers/user.controller");
+router.post("/register",controller.register);
+module.exports=router;
