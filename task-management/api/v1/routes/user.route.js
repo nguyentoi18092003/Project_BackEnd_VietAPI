ea4a944f0db5251b0daa1bc12068c0134e6112cc
Nguyen Thi Toi <nguyentoi18092003hvcnbcvt@gmail.com>
2024-04-12 02:36:21 +0700

    Buoi 49 12:46

--- task-management/api/v1/routes/user.route.js
+++ task-management/api/v1/routes/user.route.js
@@ -6,3 +6,4 @@ router.post("/register",controller.register);
 router.post("/login",controller.login);
 router.get("/detail/:id",controller.detail);
 module.exports=router;
+// chua xong ms ket thuc Trang thong tin ca nhan, Chua lam 04 Chuyen thanh cac route
